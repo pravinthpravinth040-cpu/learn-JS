f4d46ffc11152e553cdf1f986caa446bb3c596f2
Pravinth N <pravinthpravinth040@gamil.com>
2025-11-02 00:02:52 +0530

    mod

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,24 @@
-//MATH method and properties
+//If statements
 
-console.log(Math.floor(Math.random()*100)+1);
+let testScore = 65;
+let grade;
+let collegeStudent = false;
+
+if (testScore>=90) {
+    grade = `A`
+}else if (testScore>=80){
+    grade =`B`
+}else if (testScore>=70){
+    grade =`C`
+}else if (testScore>=60){
+    grade =`D`
+}
+ else{
+    if (collegeStudent) {
+        grade =`RA`
+    }else{
+        grade = `F`
+    }
+    grade = `F`
+ }   
+console.log(grade);
