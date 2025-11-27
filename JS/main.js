fb0f460e1994ed66868ab6c202e0d00075b5cb6e
Pravinth N <pravinthpravinth040@gamil.com>
2025-11-27 22:46:23 +0530

    mod

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,7 @@
-let number1 = 5;
-let number2 = 20;
+let x= 5;
 
-console.log(number1 + number2)
-console.log(number1 - number2)
-console.log(number1 * number2)
-console.log(number1 / number2)
-console.log(number1 % number2)
-console.log(number1 % number2)
-console.log(number1 ** number2)
+console.log(x < 5);
+console.log(x <= 5);
+
+console.log(x > 5);
+console.log(x >= 5);
